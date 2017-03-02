91699204922f0262058b034304c13b2493a054eb
elberto <elberto@elberto-VirtualBox>
2017-03-02 05:20:13 +0100

    second commit

---
+++ geovotedemo/src/main/java/com/geovote/GeovotedemoApplication.java
@@ -0,0 +1,12 @@
+package com.geovote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeovotedemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeovotedemoApplication.class, args);
+	}
+}
