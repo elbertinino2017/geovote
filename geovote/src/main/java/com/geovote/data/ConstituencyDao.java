3b49e8ffe842fbcdbc0c6cb2d52d75e5a0816f76
elberto <felicien.fotiomanfo@gmail.com>
2017-03-03 18:38:24 +0100

    previous message

--- geovote/src/main/java/com/geovote/data/ConstituencyDao.java
+++ geovote/src/main/java/com/geovote/data/ConstituencyDao.java
@@ -5,10 +5,11 @@ import java.util.List;
 import com.geovote.domain.Constituency;
 
 public interface ConstituencyDao {
-	
-	
+
 	public void create(Constituency constituency);
+
 	public void delete(Constituency constituency);
+
 	public List<Constituency> allConstituencies();
 
 }
